3f5ba4186c1ad01156b2a3ab5cf2ccd136063c6a
Lucas Correia <lucosil.correia@gmail.com>
2022-03-08 18:11:26 -0300

    Realização camada de service e repository

---
+++ src/main/java/com/lcorreia/cursomc/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.lcorreia.cursomc.services;
+
+import com.lcorreia.cursomc.domain.Categoria;
+import com.lcorreia.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository repo;
+
+    public Categoria buscar(Integer id) {
+
+        Optional<Categoria> obj = repo.findById(id);
+        return obj.orElse(null);
+
+    }
+
+}
