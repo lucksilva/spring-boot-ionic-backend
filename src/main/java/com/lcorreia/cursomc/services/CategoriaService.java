c59eeb4f25877b28a9be140f79ef9bc83453ccfb
Lucas Correia <lucosil.correia@gmail.com>
2022-03-14 17:21:36 -0300

    Tratando exceções (ajustando endpoint /categorias/id)

--- src/main/java/com/lcorreia/cursomc/services/CategoriaService.java
+++ src/main/java/com/lcorreia/cursomc/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.lcorreia.cursomc.services;
 
 import com.lcorreia.cursomc.domain.Categoria;
 import com.lcorreia.cursomc.repositories.CategoriaRepository;
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +17,7 @@ public class CategoriaService {
     public Categoria buscar(Integer id) {
 
         Optional<Categoria> obj = repo.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(" Objeto não encontrado! ID: " + id, Categoria.class.getName()));
 
     }
 
