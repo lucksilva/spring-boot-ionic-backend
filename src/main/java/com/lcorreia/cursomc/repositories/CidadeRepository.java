30b00848561970857dd889e1c5c8bb64a162e1dc
Lucas Correia <lucosil.correia@gmail.com>
2022-03-14 17:52:10 -0300

    Adicionando Cidade e Estado

---
+++ src/main/java/com/lcorreia/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,10 @@
+package com.lcorreia.cursomc.repositories;
+
+import com.lcorreia.cursomc.domain.Cidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}
