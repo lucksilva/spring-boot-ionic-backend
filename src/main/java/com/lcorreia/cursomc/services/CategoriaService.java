27b148226f1826b4306df8afd5d0d08b5f080dc0
Lucas Correia <lucosil.correia@gmail.com>
2022-03-16 20:50:19 -0300

    Correções: update em CategoriaService e construtor de Pagamento

--- src/main/java/com/lcorreia/cursomc/services/CategoriaService.java
+++ src/main/java/com/lcorreia/cursomc/services/CategoriaService.java
@@ -1,6 +1,7 @@
 package com.lcorreia.cursomc.services;
 
 import com.lcorreia.cursomc.domain.Categoria;
+import com.lcorreia.cursomc.domain.Cliente;
 import com.lcorreia.cursomc.dto.CategoriaDTO;
 import com.lcorreia.cursomc.repositories.CategoriaRepository;
 import com.lcorreia.cursomc.services.exceptions.DataIntegrityException;
@@ -34,8 +35,9 @@ public class CategoriaService {
     }
 
     public Categoria update(Categoria obj) {
-        find(obj.getId());
-        return repo.save(obj);
+        Categoria newObj = find(obj.getId());
+        updateData(newObj, obj);
+        return repo.save(newObj);
     }
 
     public void delete(Integer id) {
@@ -58,6 +60,10 @@ public class CategoriaService {
 
     public Categoria fromDTO(CategoriaDTO objDto){
         return new Categoria(objDto.getId(), objDto.getNome());
+    }
+
+    private void updateData(Categoria newObj, Categoria obj){
+        newObj.setNome(obj.getNome());
     }
 //
 }
