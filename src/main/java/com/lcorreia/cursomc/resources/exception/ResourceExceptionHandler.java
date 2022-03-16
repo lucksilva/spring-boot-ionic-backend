3260852b51d0ca9cf155cd3dff294eca4327f29a
Lucas Correia <lucosil.correia@gmail.com>
2022-03-16 08:43:08 -0300

    Incluindo DELETE para Categoria e tratando exceções

--- src/main/java/com/lcorreia/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/lcorreia/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.lcorreia.cursomc.resources.exception;
 
-import org.hibernate.ObjectNotFoundException;
+import com.lcorreia.cursomc.services.exceptions.DataIntegrityException;
+import com.lcorreia.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,10 +13,17 @@ import javax.servlet.http.HttpServletRequest;
 public class ResourceExceptionHandler {
 
     @ExceptionHandler(ObjectNotFoundException.class)
-    public ResponseEntity<StandardError> objectiNotFound(ObjectNotFoundException e, HttpServletRequest request){
-        StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
 
+        StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+    }
+
+    @ExceptionHandler(DataIntegrityException.class)
+    public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request){
+
+        StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 
 }
--- src/main/java/com/lcorreia/cursomc/services/CategoriaService.java
+++ src/main/java/com/lcorreia/cursomc/services/CategoriaService.java
@@ -2,8 +2,10 @@ package com.lcorreia.cursomc.services;
 
 import com.lcorreia.cursomc.domain.Categoria;
 import com.lcorreia.cursomc.repositories.CategoriaRepository;
+import com.lcorreia.cursomc.services.exceptions.DataIntegrityException;
 import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -29,6 +31,15 @@ public class CategoriaService {
     public Categoria update(Categoria obj) {
         find(obj.getId());
         return repo.save(obj);
+    }
+
+    public void delete(Integer id) {
+        find(id);
+        try {
+            repo.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+                throw new DataIntegrityException("Não é possivel excluir categorias com produtos cadastrado");
+        }
     }
 
 }
