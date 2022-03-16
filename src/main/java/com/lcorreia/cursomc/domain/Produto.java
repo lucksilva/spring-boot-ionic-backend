a478f129fc8a902930739855a64e3f82d8bc6591
Lucas Correia <lucosil.correia@gmail.com>
2022-03-15 21:47:26 -0300

    Atualizando projeto, removento JsonManegedReference e substituindo JsonBackReference por JsonIgnore

--- src/main/java/com/lcorreia/cursomc/domain/Produto.java
+++ src/main/java/com/lcorreia/cursomc/domain/Produto.java
@@ -1,6 +1,5 @@
 package com.lcorreia.cursomc.domain;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
@@ -21,7 +20,7 @@ public class Produto implements Serializable {
     @OneToMany(mappedBy = "id.produto")
     private Set<ItemPedido> itens = new HashSet<>();
 
-    @JsonBackReference
+    @JsonIgnore
     @ManyToMany
     @JoinTable( name = "PRODUTO_CATEGORIA",
                 joinColumns = @JoinColumn(name = "produto_id"),
