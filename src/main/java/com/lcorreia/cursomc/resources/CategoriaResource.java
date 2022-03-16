1c51f992a7213673fc3936e903b8349be60a0a6c
Lucas Correia <lucosil.correia@gmail.com>
2022-03-15 22:13:08 -0300

    Incluindo Verbo POST para incluir Categorias

--- src/main/java/com/lcorreia/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/lcorreia/cursomc/resources/CategoriaResource.java
@@ -4,11 +4,10 @@ import com.lcorreia.cursomc.domain.Categoria;
 import com.lcorreia.cursomc.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,6 +22,15 @@ public class CategoriaResource {
     public ResponseEntity<?> find (@PathVariable Integer id) {
         Categoria obj = service.buscar(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+        return ResponseEntity.created(uri).build();
     }
 
 }
