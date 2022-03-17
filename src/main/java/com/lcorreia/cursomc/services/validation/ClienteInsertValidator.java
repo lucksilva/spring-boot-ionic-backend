8502545217cd33a683c1d9c22882ea77e7dac038
Lucas Correia <lucosil.correia@gmail.com>
2022-03-17 09:31:42 -0300

    Inserindo validação customizada do email na inserção de cliente utilizando anotação personalizada

--- src/main/java/com/lcorreia/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/lcorreia/cursomc/services/validation/ClienteInsertValidator.java
@@ -1,9 +1,12 @@
 package com.lcorreia.cursomc.services.validation;
 
+import com.lcorreia.cursomc.domain.Cliente;
 import com.lcorreia.cursomc.domain.enums.TipoCliente;
 import com.lcorreia.cursomc.dto.ClienteNewDTO;
+import com.lcorreia.cursomc.repositories.ClienteRepository;
 import com.lcorreia.cursomc.resources.exception.FieldMassage;
 import com.lcorreia.cursomc.services.validation.utils.BR;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
@@ -11,6 +14,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+    @Autowired
+    private ClienteRepository repo;
+
     @Override
     public void initialize(ClienteInsert ann) {
     }
@@ -24,6 +31,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
         if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())){
             list.add(new FieldMassage("cpfOuCnpj", "CNPJ Invalido"));
+        }
+
+        Cliente aux = repo.findByEmail(objDto.getEmail());
+        if (aux != null){
+            list.add(new FieldMassage("email", "Email já existente"));
         }
 
         for (FieldMassage e : list) {
