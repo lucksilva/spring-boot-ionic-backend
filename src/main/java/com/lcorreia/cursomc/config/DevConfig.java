ae277d412fff58a6f8b161fa083f123f3b11ae4f
Lucas Correia <lucosil.correia@gmail.com>
2022-03-18 08:27:41 -0300

    Creando profile de desenvolvimento utilizando banco de dados mysql (pegando variavel local do profile para ordenar a estrategia de inicialização do banco de dados CREATE/NONE/CREATE-DROP/UPDATE)

---
+++ src/main/java/com/lcorreia/cursomc/config/DevConfig.java
@@ -0,0 +1,32 @@
+package com.lcorreia.cursomc.config;
+
+import com.lcorreia.cursomc.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public boolean instatiateDatabase() throws ParseException {
+
+        if (!"create".equals(strategy)){
+            return false;
+        }
+
+        dbService.instantiateTestDabaBase();
+        return true;
+    }
+}
